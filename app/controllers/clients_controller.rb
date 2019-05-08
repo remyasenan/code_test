f6075d9332a32625ef29d4adf881978ee513674d
Remya Senan <remya.senan@rubyians.com>
2019-05-08 04:04:06 +0100

    Styling of the page and in controller added request function

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,12 +8,9 @@ class ClientsController < ApplicationController
 
 		if @client.valid?
 			response = HTTParty.post(ENV['LEAD_API_URI'], {  
-		  body: "access_token=#{ENV['LEAD_API_ACCESS_TOKEN']}&pGUID=#{ENV['LEAD_API_PGUID']}" \
-		        "&pAccName=#{ENV['LEAD_API_PACCNAME']}&pPartner=#{ENV['LEAD_API_PPARTNER']}" \
-		        "&name=#{@client.full_name}&business_name=#{@client.business_name}" \
-		        "&telephone_number=#{@client.tel_num}&email=#{@client.email}", 
+		  body: request_body(@client), 
           headers: {
-            'Content-Type' => 'application/multipart_form-data',
+            'Content-Type' => 'application/x-www-form-urlencoded',
             'charset' => 'utf-8'
           }
       })
@@ -29,19 +26,29 @@ class ClientsController < ApplicationController
 
 	private
 
+     def request_body(client)
+	  	body = "access_token=#{ENV['LEAD_API_ACCESS_TOKEN']}&pGUID=#{ENV['LEAD_API_PGUID']}" \
+		        "&pAccName=#{ENV['LEAD_API_PACCNAME']}&pPartner=#{ENV['LEAD_API_PPARTNER']}" \
+		        "&name=#{client.full_name}&business_name=#{client.business_name}" \
+		        "&telephone_number=#{client.tel_num}&email=#{client.email}"
+	 end
+
 	 def interpret_response(response)
 	    error_string = "Appologies, we couldn't accept your contact request" \
 	                 " Please try again."
 	    if response.code == 201
-	      flash[:success] = "#{@client.name}, Thank you for your submission!" \
-	                        " Make it cheaper executive will contact you back soon"
-	      redirect_to root_path
+	      flash[:success] = "#{@client.full_name}, Thank you for your submission!" \
+	                        " Make it cheaper executive will contact back soon"
+	      redirect_to root_path 
 	    elsif response.code == 400
 	      flash[:danger] = "Bad Request (validation errors)"
 	      render 'new'
 	    elsif response.code == 401
 	      flash[:danger] = "Unauthorized access_token"
 	      render 'new'  
+	    elsif response.code == 404
+	      flash[:danger] = "The page you were looking for doesn't exist"
+	      render 'new'   
 	    elsif response.code == 500
 	      flash[:danger] = "Internal server error"
 	      render 'new'   
@@ -52,14 +59,6 @@ class ClientsController < ApplicationController
 
 	  end
 
-
-
-	  # def request_body
-	  # 	body = "access_token=#{ENV['LEAD_API_ACCESS_TOKEN']}&pGUID=#{ENV['LEAD_API_PGUID']}" \
-   #      "&pAccName=#{ENV['LEAD_API_PACCNAME']}&pPartner=#{ENV['LEAD_API_PPARTNER']}" \
-   #      "&name=#{customer.name}&business_name=#{customer.business_name}" \
-   #      "&telephone_number=#{customer.tel_number}&email=#{customer.email}"
-	  # end
 
 	def client_params
 		params.require(:client).permit(:full_name,:business_name,:email,:tel_num)
